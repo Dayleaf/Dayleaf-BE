baef2ff48e2a406f75915700d1366436349f83d5
ckr8305 <ckr8305@naver.com>
2026-04-06 20:12:12 +0900

    setting(#2): 프로젝트 세팅

---
+++ src/main/java/org/example/dayleaf/DayleafApplication.java
@@ -0,0 +1,13 @@
+package org.example.dayleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dayleaf/DayleafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dayleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
