0a52a8b16c335035ee925b5e0970baf433dbcbe5
kimeoyeon <djdus126@gmail.com>
2026-05-18 13:00:24 +0900

    ✨feat(#7): 스케줄 도메인 구현

---
+++ src/main/java/org/example/dayleaf/domain/schedule/entity/Schedule.java
@@ -0,0 +1,31 @@
+package org.example.dayleaf.domain.schedule.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalTime endTime;
+
+    @Column(name = "is_all_day", nullable = false)
+    private boolean isAllDay;
+}
